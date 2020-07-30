3e1f53e4ed1d48cc4fff0368ff288c68a578a660
vijay120796 <67395946+vijay120796@users.noreply.github.com>
2020-07-30 10:47:08 +0530

    HelloWorld Uploaded

---
+++ HelloWorld1/src/com/Helloworld/Application/PrintingString.java
@@ -0,0 +1,10 @@
+package com.Helloworld.Application;
+
+public class PrintingString {
+
+	public static void main(String[] args) {
+		System.out.println("New application");
+
+	}
+
+}
---
+++ HelloWorld1/src/com/Helloworld1/test/PrintHelloworld.java
@@ -0,0 +1,10 @@
+package com.Helloworld1.test;
+
+public class PrintHelloworld {
+
+	public static void main(String[] args) {
+		System.out.print("HelloWorld");
+
+	}
+
+}
